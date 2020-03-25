5ebaa98766b68c9842260b543ddf5c839012caf6
Esteban Rivas <estebanarivasv@gmail.com>
2020-03-25 00:47:26 -0300

    Creación de las carpetas api y app, entorno virtual y archivos instaladores y ejecutables

---
+++ api/app.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+app = Flask(__name__)
+
+load_dotenv()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv('PORT'))
