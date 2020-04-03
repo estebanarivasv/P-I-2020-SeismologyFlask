8cff0134b119f921ee544af21a2b0d75a0645844
Esteban Rivas <estebanarivasv@gmail.com>
2020-04-03 00:55:50 -0300

    Easy-to-export resources names referenced

---
+++ api/main/resources/__init__.py
@@ -0,0 +1,7 @@
+from .Seism import UnverifiedSeism as UnverifiedSeismResource
+from .Seism import UnverifiedSeisms as UnverifiedSeismsResource
+from .Seism import VerifiedSeism as VerifiedSeismResource
+from .Seism import VerifiedSeisms as VerifiedSeismsResource
+
+from .Sensor import Sensor as SensorResource
+from .Sensor import Sensors as SensorsResource
