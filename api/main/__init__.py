9f338e03517f3a7c822cd17c43d4064e2702ca33
Esteban <estebanarivasv@gmail.com>
2020-04-10 20:35:00 -0300

    Configuring database instance and parameters: uri, name, path

--- api/main/__init__.py
+++ api/main/__init__.py
@@ -1,10 +1,14 @@
+import os
+
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 import main.resources as resources
 
 api = Api()
+db = SQLAlchemy()
 
 
 def create_app():
@@ -12,6 +16,14 @@ def create_app():
     app = Flask(__name__)
 
     load_dotenv()
+
+    if not os.path.exists(os.getenv('SQLALCHEMY_DATABASE_PATH') + os.getenv('SQLALCHEMY_DATABASE_NAME')):
+        os.mknod(os.getenv('SQLALCHEMY_DATABASE_PATH') + os.getenv('SQLALCHEMY_DATABASE_NAME'))
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_TRACK_URI'] = 'sqlite:////' + os.getenv('SQLALCHEMY_DATABASE_PATH') + os.getenv('SQLALCHEMY_DATABASE_NAME')
+
+    db.init_app(app)
 
     api.add_resource(resources.SensorResource, '/sensor/<id_num>')
     api.add_resource(resources.SensorsResource, '/sensors')
