f837374d745b64dfb5b921d2af4b593061bd673b
Esteban <estebanarivasv@gmail.com>
2020-04-10 20:36:46 -0300

    Launching inicialized app with created modules

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from main import create_app
+from main import db
 
 import os
 
@@ -7,4 +8,5 @@ app = create_app()
 app.app_context().push()
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, port=os.getenv('PORT'))
