dfe3432527c5209f443c5ee19f7ddd63a18937e1
Esteban <estebanarivasv@gmail.com>
2020-04-10 22:20:42 -0300

    Updating User.py

--- api/main/modules/User.py
+++ api/main/modules/User.py
@@ -8,7 +8,7 @@ class User(db.Model):
     admin = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
-        return '<Professor: %r %r >' % self.email
+        return '<User: %r >' % self.email
 
     def to_json(self):
         user_json = {
