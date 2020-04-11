c7c3f4d6c9aa1ffc441ee3c5ba9f558e90e62932
Esteban <estebanarivasv@gmail.com>
2020-04-10 23:39:05 -0300

    User and Users resources created

---
+++ api/main/resources/User.py
@@ -0,0 +1,21 @@
+from flask_restful import Resource
+from flask import request
+
+
+class User(Resource):
+    def get(self):
+        """"""
+
+    def put(self):
+        """"""
+
+    def delete(self):
+        """"""
+
+
+class Users(Resource):
+    def get(self):
+        """"""
+
+    def post(self):
+        """"""
