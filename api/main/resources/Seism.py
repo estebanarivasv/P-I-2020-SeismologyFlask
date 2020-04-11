615d17a9af809c5594da6a5c4db38b1cd5830b52
Esteban <estebanarivasv@gmail.com>
2020-04-11 00:16:25 -0300

    Seism resource linked to database

--- api/main/resources/Seism.py
+++ api/main/resources/Seism.py
@@ -1,53 +1,47 @@
 from flask_restful import Resource
-from flask import request
-
-SEISMS = {
-    1: {'ref_number': '001', 'date': '11-02-2019', 'time': '20:59', 'magnitude': '5.16'},
-    2: {'ref_number': '005', 'date': '25-05-2019', 'time': '13:40', 'magnitude': '9.75'},
-    3: {'ref_number': '013', 'date': '19-08-2019', 'time': '16:36', 'magnitude': '4.63'},
-    4: {'ref_number': '019', 'date': '04-11-2019', 'time': '18:42', 'magnitude': '11.24'},
-    5: {'ref_number': '020', 'date': '16-12-2019', 'time': '07:12', 'magnitude': '6.78'},
-    6: {'ref_number': '049', 'date': '02-02-2020', 'time': '23:20', 'magnitude': '15.08'}
-}
+from flask import request, jsonify
+from .. import db
+from main.models import SeismModule
 
 
 class VerifiedSeism(Resource):
 
     def get(self, id_num):
-        if int(id_num) in SEISMS:
-            return SEISMS[int(id_num)], 200
-        return '', 404
+        verified_seism = db.session.query(SeismModule).get_or_404(id_num, verified=True)
+        return verified_seism.to_json()
 
 
 class VerifiedSeisms(Resource):
 
     def get(self):
-        return SEISMS, 200
+        verified_seisms = db.session.query(SeismModule).all(verified=True)
+        return jsonify({'verified_seisms': [verified_seism.to_json() for verified_seism in verified_seisms]})
 
 
 class UnverifiedSeism(Resource):
 
     def get(self, id_num):
-        if int(id_num) in SEISMS:
-            return SEISMS[int(id_num)], 200
-        return '', 404
-
-    def put(self, id_num):
-        if int(id_num) in SEISMS:
-            seism = SEISMS[int(id_num)]
-            data = request.get_json()
-            seism.update(data)
-            return seism, 201
-        return '', 404
+        unverified_seism = db.session.query(SeismModule).get_or_404(id_num, verified=False)
+        return unverified_seism.to_json()
 
     def delete(self, id_num):
-        if int(id_num) in SEISMS:
-            del SEISMS[int(id_num)]
-            return '', 204
-        return '', 404
+        unverified_seism = db.session.query(SeismModule).get_or_404(id_num, verified=False)
+        db.session.delete(unverified_seism)
+        db.session.commit()
+        return '', 204
+
+    def put(self, id_num):
+        unverified_seism = db.session.query(SeismModule).get_or_404(id_num, verified=False)
+        data = request.get_json().items()
+        for key, value in data:
+            setattr(unverified_seism, key, value)
+        db.session.add(unverified_seism)
+        db.session.commit()
+        return unverified_seism.to_json(), 201
 
 
 class UnverifiedSeisms(Resource):
 
     def get(self):
-        return SEISMS, 200
+        unverified_seisms = db.session.query(SeismModule).all()
+        return jsonify({'unverified_seisms': [unverified_seism.to_json() for unverified_seism in unverified_seisms]})
