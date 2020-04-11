69f0fad3324e25422a746ee93ea8679e30ed67b5
Esteban <estebanarivasv@gmail.com>
2020-04-10 23:04:03 -0300

    Updating User.py

--- api/main/modules/User.py
+++ api/main/modules/User.py
@@ -2,7 +2,7 @@ from main import db
 
 
 class User(db.Model):
-    """id_num = db.Column(db.Integer, primary_key=True)"""
+    id_num = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(30), nullable=False)
     admin = db.Column(db.Boolean, nullable=False)
@@ -12,7 +12,7 @@ class User(db.Model):
 
     def to_json(self):
         user_json = {
-            """"id_num": self.id_num"""
+            "id_num": self.id_num,
             "email": self.email,
             "password": self.password,
             "admin": self.admin
@@ -20,13 +20,13 @@ class User(db.Model):
         return user_json
 
     @staticmethod
-    def from_json(self, user_json):
-        """id_num = professor_json.get('id_num')"""
+    def from_json(user_json):
+        id_num = user_json.get('id_num')
         email = user_json.get('email')
         password = user_json.get('password')
         admin = user_json.get('admin')
         return User(
-            """id_num = id_num""",
+            id_num=id_num,
             email=email,
             password=password,
             admin=admin
