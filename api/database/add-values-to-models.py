2e586a0893cf3f20d9f8e11f1b46f01e26f9f1e0
Esteban <estebanarivasv@gmail.com>
2020-04-20 21:03:14 -0300

    Adapting script in order to work with db relations

--- api/database/add-values-to-models.py
+++ api/database/add-values-to-models.py
@@ -1,15 +1,14 @@
+import random
+import string
+from datetime import datetime
+from random import randint, uniform, getrandbits
+
 import main.models.Seism as SeismModule
 import main.models.Sensor as SensorModule
 import main.models.User as UserModule
-from main import db
 
 from app import app
-
-from random import randint, uniform, getrandbits
-from datetime import datetime
-
-"""THIS SCRIPT WAS OBTAINED FROM
-https://stackoverflow.com/questions/21014618/python-randomly-generated-ip-address-as-string"""
+from main import db
 
 
 def create_random_ip():
@@ -17,7 +16,8 @@ def create_random_ip():
     return ip
 
 
-"""END OF COPIED SCRIPT"""
+def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
 
 
 def create_random_email():
@@ -57,32 +57,33 @@ def add_random_seism_to_db():
             magnitude=round(uniform(2.0, 5.5), 1),
             latitude=uniform(-180, 180),
             longitude=uniform(-90, 90),
-            verified=bool(getrandbits(1))
+            verified=bool(getrandbits(1)),
+            sensor_id=int(input("Sensor id associated with the seism: "))
         )
-        # """sensor_id="""
 
         db.session.add(random_seism)
         db.session.commit()
 
 
-def add_random_sensor_to_db():
+def add_random_sensor_to_db(id_number):
     with app.app_context():
         random_sensor = SensorModule.Sensor(
-            id_num=randint(0, 10000000),
-            name=input("Sensor name: "),
+            id_num=id_number,
+            name=random_generator(),
             ip=create_random_ip(),
             port=randint(1, 65535),
             status=bool(getrandbits(1)),
-            active=bool(getrandbits(1))
+            active=bool(getrandbits(1)),
+            user_id=int(input("User id associated with the sensor: "))
         )
         db.session.add(random_sensor)
         db.session.commit()
 
 
-def add_random_user_to_db():
+def add_random_user_to_db(id_number):
     with app.app_context():
         random_user = UserModule.User(
-            id_num=randint(0, 10000000),
+            id_num=id_number,
             email=create_random_email(),
             password=input("Password: "),
             admin=create_user_permissions()
@@ -91,9 +92,9 @@ def add_random_user_to_db():
         db.session.commit()
 
 
-for i in range(15):
+for i in range(17):
+    add_random_user_to_db(i)
+for i in range(30):
+    add_random_sensor_to_db(i)
+for i in range(60):
     add_random_seism_to_db()
-for i in range(15):
-    add_random_sensor_to_db()
-for i in range(15):
-    add_random_user_to_db()
