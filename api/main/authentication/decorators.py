7822365780bef1e94eccc816ea08b153ad21a449
Esteban <estebanarivasv@gmail.com>
2020-05-05 01:12:13 -0300

    Minor details rearranged

--- api/main/authentication/decorators.py
+++ api/main/authentication/decorators.py
@@ -3,14 +3,14 @@ from flask_jwt_extended import verify_jwt_in_request, get_jwt_claims
 
 
 # We define this decorator in order to restrict the methods that are only accessed by the administrators
-def admin_logon_required(function):
-    @wraps(function)
+def admin_logon_required(method):
+    @wraps(method)
     def wrapper(*args, **kwargs):
-        verify_jwt_in_request()
-        claims = get_jwt_claims()
+        verify_jwt_in_request()                         # We verify if the token entered is valid
+        claims = get_jwt_claims()                       # We fetch the JWT claims
 
         if claims['admin']:
-            return function(*args, **kwargs)
+            return method(*args, **kwargs)              # If the logged user is an admin, we execute the method
         else:
             return 'You are not allowed to access to this information', 403
 
