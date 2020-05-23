eeebb6e3313da1364f6430bea63b3dc0ea98f7ff
Esteban <estebanarivasv@gmail.com>
2020-05-23 19:12:03 -0300

    Commenting files

--- api/app.py
+++ api/app.py
@@ -3,10 +3,13 @@ from main import db
 
 import os
 
+# Creating Flask app instance
 app = create_app()
 
+# Loading app context
 app.app_context().push()
 
+# If this script is run, the db is created if not; and the app is run in an specific port
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True, port=os.getenv('PORT'))
