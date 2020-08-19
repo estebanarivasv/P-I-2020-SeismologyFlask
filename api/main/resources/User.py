3c4f57b0aac557c6fccb8f63d673ca9b6b183158
Esteban Rivas <estebanarivasv@gmail.com>
2020-08-18 23:36:09 -0300

    Commenting some lines in order to make the web usable without auth

--- api/main/resources/User.py
+++ api/main/resources/User.py
@@ -8,12 +8,12 @@ from main.authentication import admin_login_required
 
 class User(Resource):
 
-    @admin_login_required
+    #@admin_login_required
     def get(self, id_num):
         user = db.session.query(UserModel).get_or_404(id_num)
         return user.to_json()
 
-    @admin_login_required
+    #@admin_login_required
     def delete(self, id_num):
         user = db.session.query(UserModel).get_or_404(id_num)
         db.session.delete(user)
@@ -24,7 +24,7 @@ class User(Resource):
             return '', 409
         return 'User deleted', 204
 
-    @admin_login_required
+    #@admin_login_required
     def put(self, id_num):
         user = db.session.query(UserModel).get_or_404(id_num)
         data = request.get_json().items()
@@ -37,18 +37,18 @@ class User(Resource):
 
 class Users(Resource):
 
-    @admin_login_required
+    #@admin_login_required
     def get(self):
         users = db.session.query(UserModel).all()
         return jsonify({'users': [user.to_json() for user in users]})
 
-    @admin_login_required
+    #@admin_login_required
     def post(self):
         user = UserModel.from_json(request.get_json())
         email_exists = db.session.query(UserModel).filter(UserModel.email == user.email).scalar() is not None
         if email_exists:
             return 'The entered email address has already been registered', 409
-        else:
+        else:            
             db.session.add(user)
             db.session.commit()
             return user.to_json(), 201
