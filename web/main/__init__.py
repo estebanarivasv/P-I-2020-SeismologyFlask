e015b19ec4a46528a380b9fb6f3b9972d9cbca02
Esteban Rivas <estebanarivasv@gmail.com>
2020-08-31 11:10:14 -0300

    Inserting new library: flask-bootstrap

--- web/main/__init__.py
+++ web/main/__init__.py
@@ -3,17 +3,19 @@ from flask import Flask
 from flask_breadcrumbs import Breadcrumbs
 from dotenv import load_dotenv
 from flask_wtf import CSRFProtect
+from flask_bootstrap import Bootstrap
 
 
 def create_app():
     app = Flask(__name__)
+    csrf = CSRFProtect(app)
+    bootstrap = Bootstrap(app)
     
     load_dotenv()
     app.config['API_URL'] = os.getenv('API_URL')
     # Gets the backend url and links it to the app's api url
     
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    csrf = CSRFProtect(app)
 
     # Blueprints import
     from main.routes import admin, login, main, seismologist, unlogged_usr
@@ -22,5 +24,8 @@ def create_app():
     app.register_blueprint(main)
     app.register_blueprint(seismologist)
     app.register_blueprint(unlogged_usr)
+    
+    csrf.init_app
+    bootstrap.init_app
 
     return app
