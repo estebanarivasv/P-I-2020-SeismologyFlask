bd4c5e3d63aa8a65ecae6cfe3350b04c00adff29
Esteban Rivas <estebanarivasv@gmail.com>
2020-10-15 11:17:02 -0300

    Importing Sensor and User components

--- web-angular/src/app/app.module.ts
+++ web-angular/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SensorComponent } from './sensor/sensor.component';
+import { UserComponent } from './user/user.component';
+import { DetailedViewComponent } from './sensor/detailed-view/detailed-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SensorComponent,
+    UserComponent,
+    DetailedViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
