1a660e61ea5629e60338fc237a1cd513db5ed0d3
Esteban Rivas <estebanarivasv@gmail.com>
2020-10-15 16:36:29 -0300

    Finish submodule detailed sensor view

--- web-angular/src/app/sensor/detailed-view/detailed-view.component.ts
+++ web-angular/src/app/sensor/detailed-view/detailed-view.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SensorModel } from 'src/app/models/sensor.model'
 
 @Component({
-  selector: 'app-detailed-view',
+  selector: 'app-sensor-view',
   templateUrl: './detailed-view.component.html',
   styleUrls: ['./detailed-view.component.scss']
 })
 export class DetailedViewComponent implements OnInit {
+
+  //Property Binding
+  //Carga el valor del profesor seleccionado en la variable professor
+  @Input() sensor: SensorModel;
+
 
   constructor() { }
 
