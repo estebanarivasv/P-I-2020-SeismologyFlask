a0143f894126c24c63f84327eb36d9e861f37791
Esteban Rivas <estebanarivasv@gmail.com>
2020-10-15 16:37:03 -0300

    Define routes

--- web-angular/src/app/app-routing.module.ts
+++ web-angular/src/app/app-routing.module.ts
@@ -1,7 +1,60 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UserComponent } from './user/user.component'
+import { SensorComponent } from './sensor/sensor.component'
+import { DetailedViewComponent } from './sensor/detailed-view/detailed-view.component'
+
+const routes: Routes = [
+
+  // Home redirection
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full',
+    data: {
+      breadcrumb: 'Home'
+    }
+  },
+
+  // Routes
+  {
+    path: '',
+    data: {
+      breadcrumb: 'Home'
+    },
+    children: [
+      // User
+      {
+        path: 'user',
+        component: UserComponent,
+        data: {
+          breadcrumb: 'User'
+        },
+        children: []
+      },
+
+      // Sensor
+      {
+        path: 'sensor',
+        component: SensorComponent,
+        data: {
+          breadcrumb: 'Sensor'
+        },
+        children: [
+          {
+            path: 'view',
+            component: DetailedViewComponent,
+            data: {
+              breadcrumb: ''
+            },
+            children: []
+          }
+        ]
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
