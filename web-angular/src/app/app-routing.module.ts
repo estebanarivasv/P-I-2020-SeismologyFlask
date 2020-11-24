c45f171a7c27b6e0ed6fc6674559478b55bdfe6d
Esteban Rivas <estebanarivasv@gmail.com>
2020-11-24 10:58:18 -0300

    Upload Angular

--- web-angular/src/app/app-routing.module.ts
+++ web-angular/src/app/app-routing.module.ts
@@ -4,52 +4,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component'
 import { SensorComponent } from './sensor/sensor.component'
 import { DetailedViewComponent } from './sensor/detailed-view/detailed-view.component'
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
   // Home redirection
-  {
-    path: '',
-    redirectTo: '/',
-    pathMatch: 'full',
-    data: {
-      breadcrumb: 'Home'
-    }
-  },
+  { path: '', redirectTo: '/home', pathMatch: 'full', data: { breadcrumb: 'Home' } },
 
   // Routes
   {
-    path: '',
-    data: {
-      breadcrumb: 'Home'
-    },
+    path: 'home', data: { breadcrumb: 'Home' }, component: HomeComponent,
     children: [
-      // User
-      {
-        path: 'user',
-        component: UserComponent,
-        data: {
-          breadcrumb: 'User'
-        },
-        children: []
+      
+      { // User
+        path: 'user', component: UserComponent, data: { breadcrumb: 'Users' }, children: []
       },
 
-      // Sensor
-      {
-        path: 'sensor',
-        component: SensorComponent,
-        data: {
-          breadcrumb: 'Sensor'
-        },
-        children: [
-          {
-            path: 'view',
-            component: DetailedViewComponent,
-            data: {
-              breadcrumb: ''
-            },
-            children: []
-          }
+      
+      { // Sensor
+        path: 'sensor', component: SensorComponent, data: { breadcrumb: 'Sensors' }, children: [
+          { path: 'view', component: DetailedViewComponent, data: { breadcrumb: '' }, children: [] }
         ]
       }
     ]
